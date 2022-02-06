88d4df9b59d2858fce7b2c559911382a4dbc3e9a
Junwoo Ji <junwoo.ji@navercorp.com>
2022-02-06 19:58:27 +0900

    feat: Footer, Header, AppLayout 컴포넌트 추가, Home 화면 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { AppLayout } from "~/components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Conduit</title>
+      </Head>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
