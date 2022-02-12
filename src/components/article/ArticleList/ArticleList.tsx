ae0a5b5e50a0baef7988f53cecad7a8e01f821f6
Junwoo Ji <junwoo.ji@navercorp.com>
2022-02-13 00:07:47 +0900

    Pagination 추가

--- src/components/article/ArticleList/ArticleList.tsx
+++ src/components/article/ArticleList/ArticleList.tsx
@@ -1,14 +1,27 @@
-import { useRecoilValueLoadable } from 'recoil';
+import { useRecoilState, useRecoilValueLoadable } from 'recoil';
 import { ArticlePreview } from '..';
-import { $aritcleList } from '~/stores';
+import { $aritcleList, $articleOffset, DEFAULT_LIMIT } from '~/stores';
+import { Pagination } from '~/components/common';
 
 export const ArticleList: React.FC = () => {
   const articleListLoadable = useRecoilValueLoadable($aritcleList);
+  const [articeOffset, setArticleOffset] = useRecoilState($articleOffset);
+
+  const handlePageChange = (page: number) => {
+    setArticleOffset(page * DEFAULT_LIMIT);
+  };
 
   switch (articleListLoadable.state) {
     case 'hasValue':
+      const { articles, articlesCount: total } = articleListLoadable.contents;
+      const showPagination = total > DEFAULT_LIMIT;
       return (
-        <>{articleListLoadable.contents.articles?.map((article) => <ArticlePreview key={article.slug} article={article} />) ?? null}</>
+        <>
+          {articles?.map((article) => <ArticlePreview key={article.slug} article={article} />) ?? null}{' '}
+          {showPagination && (
+            <Pagination total={total} limit={DEFAULT_LIMIT} currentPage={articeOffset / DEFAULT_LIMIT} onPageChange={handlePageChange} />
+          )}
+        </>
       );
     case 'loading':
       return <div>Loading...</div>;
--- src/stores/Article.store.ts
+++ src/stores/Article.store.ts
@@ -1,17 +1,25 @@
 import axios from 'axios';
-import { selector } from 'recoil';
+import { atom, selector } from 'recoil';
 import { MultipleArticlesResponse } from '~/@types/Article';
 import { $selectedTag } from '.';
+
+export const DEFAULT_LIMIT = 2;
 
 export const $aritcleList = selector<MultipleArticlesResponse>({
   key: 'articleListKey',
   get: async ({ get }) => {
-    const selectedTag = get($selectedTag);
+    const tag = get($selectedTag);
+    const offset = get($articleOffset);
     try {
-      const response = await axios.get('/api/articles', { params: { tag: selectedTag } });
+      const response = await axios.get('/api/articles', { params: { tag, offset, limit: DEFAULT_LIMIT } });
       return response.data;
     } catch (e) {
       return [];
     }
   },
+});
+
+export const $articleOffset = atom<number>({
+  key: 'articlePageKey',
+  default: 0,
 });
